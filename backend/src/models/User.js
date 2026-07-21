80e65f28ab2517e01ca6f61af3b1c1ce0a7d8b20
Muhammad Meer <sadbadbadbbadluck@gmail.com>
2026-07-21 04:46:44 -0700

    newcodee

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -28,13 +28,20 @@ const userSchema = new mongoose.Schema({
   avatar: String,
 }, { timestamps: true });
 
-// Hash password before saving
+// ✅ FIXED: Use regular function, not arrow function
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
+  // Only hash if password is modified or new
+  if (!this.isModified('password')) {
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);   // Pass error to Mongoose
+  }
 });
 
 // Method to compare password
@@ -43,4 +50,5 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
 };
 
 const User = mongoose.model('User', userSchema);
+
 module.exports = User;
